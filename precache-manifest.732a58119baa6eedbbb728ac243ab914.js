6be572dd96085aee8dd2c759735656f6e7d6a866
Irena denyshchuk <irena1811.85@gmail.com>
2023-02-03 19:54:34 +0200

    Updates

---
+++ precache-manifest.732a58119baa6eedbbb728ac243ab914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aca4adf8fa078202d42a",
+    "url": "/my-app1/static/css/main.f7a4f7bf.chunk.css"
+  },
+  {
+    "revision": "aca4adf8fa078202d42a",
+    "url": "/my-app1/static/js/main.aca4adf8.chunk.js"
+  },
+  {
+    "revision": "f99a61411f9578bd120b",
+    "url": "/my-app1/static/js/1.f99a6141.chunk.js"
+  },
+  {
+    "revision": "59fb2da9ea58e60a6ddd",
+    "url": "/my-app1/static/js/runtime~main.59fb2da9.js"
+  },
+  {
+    "revision": "c18cedc82cc379b9e49c6ab40538b48e",
+    "url": "/my-app1/index.html"
+  }
+];
